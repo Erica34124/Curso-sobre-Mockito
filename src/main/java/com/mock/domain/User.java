38d44b24f2a6dac6436271b32e7535ac7244046f
Erica Silva <Erica.Rodrigues-ext@ab-Inbev.com>
2023-03-29 21:47:57 -0300

    Create Domain class

---
+++ src/main/java/com/mock/domain/User.java
@@ -0,0 +1,24 @@
+package com.mock.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+    private String senha;
+}
